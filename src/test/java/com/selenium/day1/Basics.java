b269ad312268bb8a7f5502dbcdd8ee4081340156
unknown <shoaib786asme@gmail.com>
2025-02-20 11:45:59 +0530

    Basics of automation testing , webDriver methods)

---
+++ src/test/java/com/selenium/day1/Basics.java
@@ -0,0 +1,22 @@
+package com.selenium.day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basics {
+
+   public static void main(String[] args) throws InterruptedException {
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://practice.expandtesting.com/");
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+	Thread.sleep(3000);
+	driver.manage().window().minimize();
+	Thread.sleep(5000);
+	driver.navigate().refresh();
+	driver.navigate().back();
+	Thread.sleep(3000);
+	driver.navigate().forward();
+	driver.close();
+}
+}
