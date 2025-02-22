88ce1727c5325daa68ef65c0617be026ae03fb1f
unknown <shoaib786asme@gmail.com>
2025-02-22 12:17:58 +0530

    Locators done

--- src/test/java/com/selenium/day1/Basics.java
+++ src/test/java/com/selenium/day1/Basics.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Basics {
-
+  /** Practice (any question let me know ) */
+ /*code --> 
+  * https://github.com/Shoaibsheikh786/AutmationTesting209101112.git 
+  * */
    public static void main(String[] args) throws InterruptedException {
 	WebDriver driver= new ChromeDriver();
-	driver.get("https://practice.expandtesting.com/");
 	driver.get("https://www.google.com");
 	driver.manage().window().maximize();
 	Thread.sleep(3000);
