2119e53c9d18976c9e5032ab8273a0962057c870
unknown <shoaib786asme@gmail.com>
2025-02-24 12:36:07 +0530

    radio buttons and checkboxes

---
+++ src/test/java/com/selenium/day3/RadioButtons.java
@@ -0,0 +1,16 @@
+package com.selenium.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtons {
+
+	public static void main(String[] args) {
+    
+		WebDriver driver=new ChromeDriver();
+	    driver.get("https://practice.expandtesting.com/radio-buttons");
+	    driver.findElement(By.xpath("//input[@id='yellow']")).click();
+	}
+
+}
